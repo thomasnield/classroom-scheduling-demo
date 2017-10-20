2afc2fedf29a23a60be348531b8314b943ac2dcd
Thomas Nield <thomasnield@live.com>
2017-10-20 14:27:07 -0500

    Update KotlinSolution.kt

--- KotlinSolution.kt
+++ KotlinSolution.kt
@@ -1,47 +1,39 @@
-import com.swa.np.common.util.asMinutes
 import org.ojalgo.optimisation.ExpressionsBasedModel
 import org.ojalgo.optimisation.Variable
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.LocalTime
 
-val operatingDaysOfWeek = 1..6
+// Any Monday through Friday date range will work
+val operatingDates = LocalDate.of(2017,10,16)..LocalDate.of(2017,10,20)
 
 val operatingTimes = listOf(
         LocalTime.of(8,0)..LocalTime.of(11,0),
         LocalTime.of(13,0)..LocalTime.of(17,0)
 )
 
-// break up week into discrete 15 minute increments
-val incrementedTimeline = 0..(7 * 24 * 4)
 
+val calendarStart = operatingDates.start.with(DayOfWeek.MONDAY)
 
-// convert operating times to discrete 15 minute increments
-val availableBlocks = operatingTimes.asSequence()
-        .flatMap { timeRange ->
+// Operating DateTimes
+val availableBlocks = generateSequence(operatingDates.start) {
+    it.plusDays(1).takeIf { it <= operatingDates.endInclusive }
+}.flatMap { dt ->
+    operatingTimes.asSequence()
+            .map { dt.atTime(it.start)..dt.atTime(it.endInclusive) }
+}.map { AvailableBlock(it) }
+ .toList()
 
-            val timeRangeConverted = timeRange.start.toDiscreteIntervals()..timeRange.endInclusive.toDiscreteIntervals()
-
-            incrementedTimeline.asSequence()
-                    .filter { (it / 96) in operatingDaysOfWeek }
-                    .filter { (it % 96) in timeRangeConverted }
-                    .groupBy { it / 96 }
-                    .asSequence()
-                    .map { AvailableBlock(it.key, it.value.min()!!..it.value.max()!!, timeRange) }
-        }
-        .sortedBy { it.discreteRange.start }
-        .toList()
 
 // declare model
 val model = ExpressionsBasedModel()
 
 fun main(args: Array<String>) {
-    availableBlocks.forEach { println(it) }
+
 }
 
-fun LocalTime.toDiscreteIntervals() = asMinutes().toInt() / 15
-
-data class AvailableBlock(val dayOfWeek: Int,
-                          val discreteRange: IntRange,
-                          val timeRange: ClosedRange<LocalTime> ) {
+data class AvailableBlock(val dateTimeRange: ClosedRange<LocalDateTime>) {
 
 }
 
