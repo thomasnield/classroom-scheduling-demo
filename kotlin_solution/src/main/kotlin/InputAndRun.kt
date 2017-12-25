309a371596275ed67efd23a7fdac0f73d9ec6925
Thomas Nield <thomasnield@live.com>
2017-12-25 15:05:08 -0600

    wip

--- kotlin_solution/src/main/kotlin/InputAndRun.kt
+++ kotlin_solution/src/main/kotlin/InputAndRun.kt
@@ -18,13 +18,13 @@ val scheduledClasses = listOf(
         ScheduledClass(id=1, name="Psych 101", hoursLength=1.0, repetitions=2),
         ScheduledClass(id=2, name="English 101", hoursLength=1.5, repetitions=3),
         ScheduledClass(id=3, name="Math 300", hoursLength=1.5, repetitions=2),
-        ScheduledClass(id=4, name="Psych 300", hoursLength=3.0, repetitions=1),
+        ScheduledClass(id=4, name="Psych 300", hoursLength=3.0, repetitions=1)/*,
         ScheduledClass(id=5, name="Calculus I", hoursLength=2.0, repetitions=2),
         ScheduledClass(id=6, name="Linear Algebra I", hoursLength=2.0, repetitions=3),
         ScheduledClass(id=7, name="Sociology 101", hoursLength=1.0, repetitions=2),
-        ScheduledClass(id=8, name="Biology 101", hoursLength=1.0, repetitions=2)
+        ScheduledClass(id=8, name="Biology 101", hoursLength=1.0, repetitions=2)*/
 )
-
+gi
 
 fun main(args: Array<String>) {
     println("Job started at ${LocalTime.now()}")
