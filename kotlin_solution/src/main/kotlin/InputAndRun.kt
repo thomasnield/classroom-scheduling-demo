f76f66370aceea448430a1f6d4be9b15414535e1
Thomas Nield <thomasnield@live.com>
2018-01-08 14:18:55 -0600

    finally getting somewhere

--- kotlin_solution/src/main/kotlin/InputAndRun.kt
+++ kotlin_solution/src/main/kotlin/InputAndRun.kt
@@ -14,14 +14,14 @@ val breaks = listOf<ClosedRange<LocalTime>>(
 
 // classes
 val scheduledClasses = listOf(
-        ScheduledClass(id=1, name="Psych 101",hoursLength=1.0, repetitions=2)/*,
+        ScheduledClass(id=1, name="Psych 101",hoursLength=1.0, repetitions=2),
         ScheduledClass(id=2, name="English 101", hoursLength=1.5, repetitions=3),
         ScheduledClass(id=3, name="Math 300", hoursLength=1.5, repetitions=2),
         ScheduledClass(id=4, name="Psych 300", hoursLength=3.0, repetitions=1),
         ScheduledClass(id=5, name="Calculus I", hoursLength=2.0, repetitions=2),
         ScheduledClass(id=6, name="Linear Algebra I", hoursLength=2.0, repetitions=3),
         ScheduledClass(id=7, name="Sociology 101", hoursLength=1.0, repetitions=2),
-        ScheduledClass(id=8, name="Biology 101", hoursLength=1.0, repetitions=2)*/
+        ScheduledClass(id=8, name="Biology 101", hoursLength=1.0, repetitions=2)
 )
 
 fun main(args: Array<String>) {
@@ -32,8 +32,8 @@ fun main(args: Array<String>) {
 
     println(model.minimise())
 
-    Session.all.forEach {
-        println("${it.name}-${it.repetitionIndex}: ${it.start.dayOfWeek} ${it.start.toLocalTime()}-${it.end.toLocalTime()}")
+    ScheduledClass.all.forEach {
+        println("${it.name}- ${it.daysOfWeek.joinToString("/")} ${it.start.toLocalTime()}-${it.end.toLocalTime()}")
     }
 
     println("Job ended at ${LocalTime.now()}")
--- kotlin_solution/src/main/kotlin/ScheduleEntities.kt
+++ kotlin_solution/src/main/kotlin/ScheduleEntities.kt
@@ -42,23 +42,9 @@ data class ScheduledClass(val id: Int,
                           val name: String,
                           val hoursLength: Double,
                           val repetitions: Int,
-                          val repetitionGap: Int = 48 * 4) {
+                          val repetitionGapDays: Int = 2) {
 
-    val sessions by lazy {
-        Session.all.filter { it.parentClass == this }
-    }
-
-    companion object {
-        val all by lazy { scheduledClasses }
-    }
-}
-
-
-data class Session(val id: Int,
-                   val name: String,
-                   val hoursLength: Double,
-                   val repetitionIndex: Int,
-                   val parentClass: ScheduledClass) {
+    val repetitionGapSlots = repetitionGapDays * 24 * 4
 
     val slotsNeeded = (hoursLength * 4).toInt()
 
@@ -69,18 +55,18 @@ data class Session(val id: Int,
     val start get() = slots.asSequence().first { it.occupied.value.toInt() == 1 }.block.dateTimeRange.start
     val end get() = start.plusMinutes((hoursLength * 60.0).toLong())
 
+    val daysOfWeek get() = (0..(repetitions-1)).asSequence().map { start.dayOfWeek.plus(it.toLong() * repetitionGapDays) }.sorted()
+
     fun addConstraints() {
 
         //block out exceptions
-        addExpression().level(0).apply {
-            slots.asSequence()
-                    .filter { os -> breaks.any { os.block.timeRange.start in it } || os.block.timeRange.start !in operatingDay }
-                    .forEach {
-                        // b = 0, where b is occupation state of slot
-                        // this means it should never be occupied
-                        set(it.occupied, 1)
-                    }
-        }
+        slots.asSequence()
+                .filter { os -> breaks.any { os.block.timeRange.start in it } || os.block.timeRange.start !in operatingDay }
+                .forEach {
+                    // b = 0, where b is occupation state of slot
+                    // this means it should never be occupied
+                    it.occupied.lower(0).upper(0)
+                }
 
         //sum of all boolean states for this session must be 1
         addExpression().level(1).apply {
@@ -90,7 +76,7 @@ data class Session(val id: Int,
         }
 
         //handle contiguous states
-        slots.rollingRecurrences(slotsNeeded = slotsNeeded, gapSize = parentClass.repetitionGap, recurrencesNeeded = parentClass.repetitions)
+        slots.rollingRecurrences(slotsNeeded = slotsNeeded, gapSize = repetitionGapSlots, recurrencesNeeded = repetitions)
                 .forEach { batch ->
                     val flattenedBatch = batch.flatMap { it }
                     val first = flattenedBatch.first()
@@ -104,23 +90,28 @@ data class Session(val id: Int,
                         set(first.block.cumulativeState, -1)
                     }
                 }
+
+        //prevent scheduling past end of window
+        slots.takeLast((repetitionGapSlots * (repetitions - 1)).let { if (it > 0) it - 1 else it }).forEach {
+            it.occupied.lower(0).upper(0)
+        }
     }
 
     companion object {
-        val all by lazy {
-            ScheduledClass.all.map { Session(it.id, it.name, it.hoursLength, 1, it) }
-        }
+        val all by lazy { scheduledClasses }
     }
 }
 
-data class Slot(val block: Block, val session: Session) {
+
+
+data class Slot(val block: Block, val session: ScheduledClass) {
     val occupied = variable().binary()
 
     companion object {
 
         val all by lazy {
             Block.all.asSequence().flatMap { b ->
-                Session.all.asSequence().map { Slot(b,it) }
+                ScheduledClass.all.asSequence().map { Slot(b,it) }
             }.toList()
         }
     }
@@ -128,7 +119,7 @@ data class Slot(val block: Block, val session: Session) {
 
 
 fun applyConstraints() {
-    Session.all.forEach { it.addConstraints() }
+    ScheduledClass.all.forEach { it.addConstraints() }
 }
 
 fun <T> List<T>.rollingBatches(batchSize: Int) = (0..size).asSequence().map { i ->
